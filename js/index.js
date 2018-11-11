a4de6660a21e35cddb815cf3f842b9a6b709f9e0
jaimin1224 <marfatiajaimin@gmail.com>
2018-11-10 20:35:46 -0500

    Setting up variables, defined strict, on and start button event

---
+++ js/index.js
@@ -0,0 +1,46 @@
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector("#turn");
+const topLeft = document.querySelector("#topLeft");
+const topRight = document.querySelector("#topRight");
+const bottomLeft = document.querySelector("#bottomLeft");
+const bottomRight = document.querySelector("#bottomRight");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
+
+strictButton.addEventListener('click', event => {
+    if (strictButton.checked == true) {
+        strict = true;
+    } else {
+        strict = false;
+    }
+});
+
+onButton.addEventListener('click', (event) => {
+    if (onButton.checked == true) {
+        on = true;
+        turnCounter.innerHTML = "-";
+    } else {
+        on = false;
+        turnCounter.innerHTML = "";
+        clearColor();
+        clearInterval(intervalId);
+    }
+});
+
+startButton.addEventListener('click', (event) => {
+    if (on || win) {
+        play();
+    }
+});
